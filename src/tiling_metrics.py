55c0c1f307c43e12283dc50aabbeb21a82667b4b
dan_hawkley <consensusmechanics@gmail.com>
2026-05-03 12:13:06 -0600

    Update tiling_metrics.py

--- src/tiling_metrics.py
+++ src/tiling_metrics.py
@@ -1,1 +1,7 @@
-# src/tiling_metrics.py
+def lane_density(nums, mod=30):
+    lanes = {}
+    for n in nums:
+        r = n % mod
+        lanes[r] = lanes.get(r, 0) + 1
+    total = len(nums)
+    return {k: v/total for k, v in lanes.items()}
