6d47afaa0dab09b9ab41ded33121ef2d170fa4f8
dan_hawkley <consensusmechanics@gmail.com>
2026-05-03 12:16:25 -0600

    Update mod30.py

--- src/mod30.py
+++ src/mod30.py
@@ -1,3 +1,12 @@
+# src/mod30.py
+
+MOD30_RESIDUES = [1, 7, 11, 13, 17, 19, 23, 29]
+
+def mod30_index(n):
+    return n % 30
+
+def mod30_mask(n):
+    return mod30_index(n) in MOD30_RESIDUES
+
 def mod30_residues(n_max):
-    residues = [1,7,11,13,17,19,23,29]
-    return [n for n in range(2, n_max) if n % 30 in residues]
+    return [n for n in range(2, n_max) if mod30_mask(n)]
