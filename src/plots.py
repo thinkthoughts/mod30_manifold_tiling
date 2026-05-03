31b68a064bebdbea6520eb8508aa118b1fb2ab5a
dan_hawkley <consensusmechanics@gmail.com>
2026-05-03 12:13:31 -0600

    Update plots.py

--- src/plots.py
+++ src/plots.py
@@ -1,1 +1,10 @@
-# src/plots.py
+import matplotlib.pyplot as plt
+
+def plot_residue_distribution(density_dict):
+    x = list(density_dict.keys())
+    y = list(density_dict.values())
+    plt.bar(x, y)
+    plt.xlabel("Residue class")
+    plt.ylabel("Density")
+    plt.title("Mod30 residue density")
+    plt.show()
